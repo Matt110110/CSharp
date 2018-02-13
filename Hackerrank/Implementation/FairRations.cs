e28bc4060ac3fe249ace3b3dc59742551166c107
Matt110110 <matruprasadchand@gmail.com>
2018-02-14 00:10:20 +0530

    Well that worked, somehow

---
+++ Hackerrank/Implementation/FairRations.cs
@@ -0,0 +1,52 @@
+using static System.Console;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MagicSquare
+{
+    class Program
+    {
+        static int res = 0;
+        static int fairRations(int[] B)
+        {
+            // Complete this function
+            var count = B.Count(x => x % 2 != 0);
+            if (count % 2 != 0)
+            {
+                return 1;
+            }
+            else
+            {
+                for (var i = 0; i < B.Length; i++)
+                {
+                    if (B[i]%2 != 0)
+                    {
+                        B[i]++;
+                        B[i + 1]++;
+                        res += 2;
+                    }
+                }
+            }
+            return res;
+        }
+
+        static void Main(string[] args)
+        {
+            var N = int.Parse(ReadLine());
+            string[] B_temp = ReadLine().Split(' ');
+            int[] B = Array.ConvertAll(B_temp, int.Parse);
+            var result = fairRations(B);
+            if (result == 1)
+            {
+                WriteLine("NO");
+            }
+            else
+            {
+                WriteLine(result);
+            }            
+        }
+    }
+}
